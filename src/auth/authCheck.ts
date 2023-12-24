b9c9b0813b75e6b8844b77391922dd78ab351800
Angel S. Moreno <angelxmoreno@gmail.com>
2023-12-24 02:43:10 -0500

    feat(Auth): created auth check functions and base Auth Strategy class

---
+++ src/auth/authCheck.ts
@@ -0,0 +1,33 @@
+import appConfig from '@app/config';
+import { UserEntity } from '@app/entities/UserEntity';
+import Env from '@app/utils/Env';
+import { Action } from 'routing-controllers';
+import { AuthorizationChecker } from 'routing-controllers/types/AuthorizationChecker';
+import { CurrentUserChecker } from 'routing-controllers/types/CurrentUserChecker';
+
+const getTokenFromAction = (action: Action): string | null => {
+    const authorizationHeader: string | undefined = action.request.headers['authorization'];
+    if (authorizationHeader) {
+        return authorizationHeader.split('Bearer ').pop() || null;
+    }
+    return action.request.query['token'] || null;
+};
+
+const getUserFromToken = (token: string): Promise<UserEntity | boolean> => {
+    if (Env.isDev() && token === 'TEST_TOKEN') {
+        return UserEntity.findOneByOrFail({ email: appConfig.auth.testUserEmail });
+    }
+
+    return UserEntity.fromJwtToken(token);
+};
+
+export const currentUserChecker: CurrentUserChecker = async (action: Action) => {
+    const token = getTokenFromAction(action);
+
+    return token ? await getUserFromToken(token) : null;
+};
+
+export const authorizationChecker: AuthorizationChecker = async (action: Action, roles: string[]) => {
+    const token = getTokenFromAction(action);
+    return token ? !!(await getUserFromToken(token)) : false;
+};
---
+++ src/auth/strategies/StrategyBase.ts
@@ -0,0 +1,11 @@
+import { UserEntity } from '@app/entities/UserEntity';
+import { Request } from 'express';
+
+export type StrategyResponse = {
+    user?: UserEntity;
+    error?: Error;
+};
+export default interface StrategyBase<Config = object> {
+    config: Config;
+    userFromRequest: (req: Request) => Promise<StrategyResponse>;
+}
