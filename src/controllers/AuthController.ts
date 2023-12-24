54ff12d3ef1f31cc790bcce408684ba63a0a2ce0
Angel S. Moreno <angelxmoreno@gmail.com>
2023-12-24 18:46:45 -0500

    feat: created google auth endpoints

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,23 +1,40 @@
+import StrategyBase from '@app/auth/strategies/StrategyBase';
 import { UserEntity } from '@app/entities/UserEntity';
 import JwtService from '@app/services/JwtService';
+import { Request } from 'express';
 import { JsonController, UnauthorizedError } from 'routing-controllers';
 
 @JsonController('/auth')
 export default class AuthController {
-    renderAuthSuccess(user: UserEntity) {
+    protected async userFromRequest(strategy: StrategyBase, req: Request) {
+        let authError: Error | undefined = undefined;
+        try {
+            const { user, error } = await strategy.userFromRequest(req);
+            if (user) {
+                return this.renderAuthSuccess(user);
+            }
+            authError = error || new Error('Unknown Error');
+        } catch (e) {
+            authError = e as Error;
+        }
+
+        this.throwAuthError(authError);
+    }
+
+    protected renderAuthSuccess(user: UserEntity) {
         const token = JwtService.fromUser(user);
 
         return {
             token,
             user: {
                 id: user.id,
-                username: user.username,
-                avatar: user.avatar_url_prefix + 'png',
+                name: user.name,
+                avatar: user.profile_img_url,
             },
         };
     }
 
-    throwAuthError(e: Error) {
+    protected throwAuthError(e: Error) {
         const { message } = e as Error;
         throw new UnauthorizedError(message);
     }
